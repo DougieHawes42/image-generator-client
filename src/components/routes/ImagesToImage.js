3737f185b3a5c003d29c7aae146d42efd14db74e
DougieHawes42 <doughawes42@gmail.com>
2026-08-25 20:02:34 +0100

    added to the images to image route, created uploaded images gallery

--- src/components/routes/ImagesToImage.js
+++ src/components/routes/ImagesToImage.js
@@ -11,8 +11,21 @@ import { GenerateRoute } from "../utils/routes.js";
 const ImagesToImage = () => {
   const [value, setValue] = useState("");
 
+  const imageUpload = (
+    <div className="images-to-image-image-upload">
+      <label className="images-to-image-image-upload-label">
+        Upload Images
+      </label>
+      <div className="images-to-image-image-upload-input">
+        <input type="file" />
+      </div>
+      <div className="images-to-image-images-preview"></div>
+    </div>
+  );
+
   return (
     <GenerateRoute
+      imageUpload={imageUpload}
       title="Images to Image"
       subtitle="enter a prompt and an {image/some images} to generate a new image"
       name="prompt"
--- src/components/utils/routes.js
+++ src/components/utils/routes.js
@@ -24,6 +24,8 @@ export const GenerateRoute = ({
   setQuality,
   quantity,
   setQuantity,
+
+  imageUpload,
 }) => {
   return (
     <div className="generate-route">
@@ -38,40 +40,45 @@ export const GenerateRoute = ({
           onChange={onChange}
           placeholder={placeholder}
         />
-      </div>
-      <div className="generate-route-options-selectors">
-        <PromptOptionsSelector
-          value={size}
-          onChange={(e) => setSize(e.target.value)}
-          label="size"
-          items={[
-            { key: 1, value: "1024x1024", text: "square" },
-            { key: 2, value: "1536x1024", text: "landscape" },
-            { key: 3, value: "1024x1536", text: "portrait" },
-          ]}
-        />
-        <PromptOptionsSelector
-          value={quality}
-          onChange={(e) => setQuality(e.target.value)}
-          label="quality"
-          items={[
-            { key: 1, value: "low", text: "low" },
-            { key: 2, value: "medium", text: "medium" },
-            { key: 3, value: "high", text: "high" },
-          ]}
-        />
-        <PromptOptionsSelector
-          value={quantity}
-          onChange={(e) => setQuantity(Number(e.target.value))}
-          label="quantity"
-          items={[
-            { key: 1, value: 1, text: 1 },
-            { key: 2, value: 2, text: 2 },
-            { key: 3, value: 4, text: 4 },
-            { key: 4, value: 8, text: 8 },
-            { key: 5, value: 16, text: 16 },
-          ]}
-        />
+        {imageUpload && (
+          <div className="generate-route-image-upload-container">
+            {imageUpload}
+          </div>
+        )}
+        <div className="generate-route-options-selectors">
+          <PromptOptionsSelector
+            value={size}
+            onChange={(e) => setSize(e.target.value)}
+            label="size"
+            items={[
+              { key: 1, value: "1024x1024", text: "square" },
+              { key: 2, value: "1536x1024", text: "landscape" },
+              { key: 3, value: "1024x1536", text: "portrait" },
+            ]}
+          />
+          <PromptOptionsSelector
+            value={quality}
+            onChange={(e) => setQuality(e.target.value)}
+            label="quality"
+            items={[
+              { key: 1, value: "low", text: "low" },
+              { key: 2, value: "medium", text: "medium" },
+              { key: 3, value: "high", text: "high" },
+            ]}
+          />
+          <PromptOptionsSelector
+            value={quantity}
+            onChange={(e) => setQuantity(Number(e.target.value))}
+            label="quantity"
+            items={[
+              { key: 1, value: 1, text: 1 },
+              { key: 2, value: 2, text: 2 },
+              { key: 3, value: 4, text: 4 },
+              { key: 4, value: 8, text: 8 },
+              { key: 5, value: 16, text: 16 },
+            ]}
+          />
+        </div>
       </div>
       <div className="generate-route-submit-container">
         <SubmitButton onClick={onSubmit} text={buttonText} />
